e42520fadf9adbb9afb2afb9fd7d052b386d9634
Thalles <131490068+Roife07@users.noreply.github.com>
2024-10-01 08:13:46 -0300

    Create Main.java

---
+++ aluguelquadra/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Locacao locacao = new Locacao();
+        
+        locacao.cadastrarLocacao();
+        
+    }
+    
+}
